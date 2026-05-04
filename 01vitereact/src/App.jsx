bc544fff2a84acbc24b6c09f2ddf7bf5062cab06
Siddharth Rashiwala <srashiwala@yahoo.in>
2026-05-04 08:19:21 +0530

    redux toolkit completed

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 
 function App() {
+  const arr = [
+    "apple",
+    "banana",
+    "orange",
+    "pineapple",
+    "guava",
+    "chiku",
+    "grapes",
+  ];
+  const [input, setInput] = useState("");
+  const filter = arr.filter((item) => (item.includes(input)))
+  
 
   return (
-    <h1>Chai aur code</h1>
-  )
+    <>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {filter.map((item) => (
+        <li>{item}</li>
+      ))}
+    </>
+  );
 }
 
-export default App
+export default App;
--- customReact/customreact.js
+++ customReact/customreact.js
@@ -2,7 +2,9 @@ function customRender(reactElement, container) {
   // const domElement = document.createElement(reactElement.type);
   // domElement.innerHTML = reactElement.children;
   // domElement.setAttribute("href", reactElement.props.href)
+  // domElement.setAttribute("target", reactElement.props.target)
   // container.appendChild(domElement)
+  // Problem in above code is that if there are more attribute then we have to set each one separately
 
   const domElement = document.createElement(reactElement.type)
   domElement.innerHTML = reactElement.children
